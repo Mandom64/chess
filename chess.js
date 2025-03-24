b1ee0874915d7f18ba56834aadba716a16b6f0e4
Mandom64 <xristos980@gmail.com>
2025-03-24 18:23:02 +0200

    board is alive

---
+++ chess.js
@@ -0,0 +1,32 @@
+const canvas = document.getElementById("board");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+let b = [];
+let rows = 8;
+let cols = 8;
+
+function drawBoard() {
+    let squareWidth = canvas.width / rows;
+    let squareHeight = canvas.height / cols;
+
+    for (let row = 0; row < rows; row++) {
+        for(let col = 0; col < cols; col++) {
+            if((row+col) % 2 == 0) {
+                ctx.fillStyle = "white";
+            } else {
+                ctx.fillStyle = "black";
+            }
+            ctx.fillRect(col * squareWidth, row * squareHeight, squareWidth, squareHeight);
+        }
+    }
+}
+
+drawBoard();
+
+b[]
+function drawPieces() {
+
+}
