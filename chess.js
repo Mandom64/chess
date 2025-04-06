5388d6ed036deaad82fa71fdb822c061654ecbd3
Mandom64 <xristos980@gmail.com>
2025-04-06 14:21:03 +0300

    yes

--- chess.js
+++ chess.js
@@ -1,9 +1,21 @@
 /*
-    Main script for the app, might break it into more files later 
+    Main script for the app, might break it into more files later
+    TODO:
+        * Complete FEN coding, decoding
+        * Move generation
 */
 const canvas = document.getElementById("Board");
 const ctx = canvas.getContext("2d");
 
+class Position {
+    constructor(col, row) {
+        this.col = col;
+        this.row = row;
+    }
+}
+
+/* Columns are accessed in reverse like so 'board.cols - y', 
+   because im using extended algebraic notation for the moves */
 class Board {
     constructor() {
         this.square = [];
@@ -12,17 +24,9 @@ class Board {
     }
     
     Init() {
-        for(let row = 0; row < board.rows; row++) {
+        for(let row = 0; row < this.rows; row++) {
             this.square[row] = new Array(board.cols);
             this.square[row].fill("0");
-        }
-    }
-
-    Update(pieces) {
-        for(let piece of pieces) {
-            let row = piece.position.x;
-            let col = piece.position.y
-            this.square[row][col] = piece.type;
         }
     }
 
@@ -31,15 +35,106 @@ class Board {
         console.log(this.square);
     }
 
-    Move(row, col) {
+    LongNotation(from, to) {
+        return 
+    }
+
+    /* 
+
+    */
+    isValidMove(str) {
+        let from = new Position(Letters[str[0]], board.cols - str[1]);
+        let to = new Position(Letters[str[2]], board.cols - str[3]);
+        let piece = this.square[from.row][from.col]; 
+
         
+        if(to.row < 0 || to.row > board.rows || 
+           to.col < 0 || to.col > board.cols) {
+            console.log("Error: move " + str + "is illegal!");
+            return;
+        }
+
+        console.log(piece, from, this.square[from.row][from.col]);
+        console.log(piece, to, this.square[to.row][to.col]);
+        console.log("Abs: "+ Math.abs(from.row - to.row));
+
+        switch (piece) {
+            case "p":
+                /* If no piece is forward pawn can move */
+                if(this.square[to.row][to.col] == "0") {
+                    if(from.row == 1 && Math.abs(from.row - to.row) <= 2)
+                        return true;
+                    if(Math.abs(from.row - to.row) == 1)
+                        return true;
+                }
+                break;
+            case "r":
+
+                return true;
+                break;
+            case "n":
+                return true;
+                break;
+            case "b":
+                return true;
+                break;
+            case "q":
+                return true;
+                break;
+            case "k":
+                return true;
+                break;
+            case "P":
+                console.log("piece selected is: "+ piece)
+                if(this.square[to.row][to.col] == "0" && Math.abs(from.row - to.row) == 1)
+                    return true;
+                break;
+            case "R":
+                return true;
+                break;
+            case "N":
+                return true;
+                break;
+            case "B":
+                return true;
+                break;
+            case "Q":
+                return true;
+                break;
+            case "K":
+                return true;
+                break;
+        }    
+        return false;
+    }
+
+    Move(str) {
+        let from = new Position(Letters[str[0]], board.cols - str[1]);
+        let to = new Position(Letters[str[2]], board.cols - str[3]);
+        
+        if(!this.isValidMove(str))
+        {
+            console.log("Error: Move " + str + " is not valid.");
+            return;
+        }
+
+        this.square[to.row][to.col] = this.square[from.row][from.col];
+        this.square[from.row][from.col] = "0";
     }
 }
 
 /* Globals */
 let board = new Board(); /* Internal representation of the board */
-let pieces = []; /* List of all the pieces currently on the board */
-let FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"; /* Starting position*/
+let FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"; /* Starting position*/
+let activeColor;
+let castling = {
+    k: 0,
+    q: 0,
+    K: 0,
+    Q: 0,
+}
+let halfmoves;
+let fullmoves = 1;
 
 const PieceImages = {
     ["k"]: "https://upload.wikimedia.org/wikipedia/commons/f/f0/Chess_kdt45.svg",
@@ -54,12 +149,21 @@ const PieceImages = {
     ["B"]: "https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg",
     ["N"]: "https://upload.wikimedia.org/wikipedia/commons/7/70/Chess_nlt45.svg",
     ["P"]: "https://upload.wikimedia.org/wikipedia/commons/4/45/Chess_plt45.svg",
-    
 }
 
-/* TODO: Needs simplifying */
+const Letters = {
+    ["a"]: 0, ["b"]: 1, ["c"]: 2, ["d"]: 3, 
+    ["e"]: 4, ["f"]: 5, ["g"]: 6, ["h"]: 7
+}
+
+function Init(){
+    board.Init();
+    loadFEN();
+}
+
 function loadFEN() {
-    let rank = FEN.split("/");
+    let fields = FEN.split(" ");
+    let rank = fields[0].split("/");
 
     for (let row = 0; row < board.rows; row++) {
         let col = 0;
@@ -75,12 +179,25 @@ function loadFEN() {
             col++;
         }
     }
+
+    activeColor = fields[1];
+
+    if(fields[2] == "-") {
+        castling.k = castling.q = castling.K = castling.Q = 0;
+    } else {
+        for(i = 0; i < fields[2].length; i++) {
+            switch (fields[2].charAt(i)) {
+                case "k": castling.k = 1; break;
+                case "q": castling.q = 1; break;
+                case "K": castling.K = 1; break;
+                case "Q": castling.Q = 1; break;
+            }
+        }
+    }
 }
 
 function genFEN() {
     let FEN = "";
-    board.Update(pieces);
-    board.Display();
     
     for(let row = 0; row < board.rows; row++) {
         for(let col = 0; col < board.cols; col++) {
@@ -100,14 +217,14 @@ function genFEN() {
                 FEN += board.square[row][col];
             }
         }
-        FEN += "/";
+        if(row + 1 < board.rows)
+            FEN += "/";
     }
     console.log(FEN);
-
+    return FEN;
 }
 
 function drawBoard() {
-    /* Checker Pattern */
     let rectW = canvas.width / board.rows;
     let rectH = canvas.height / board.cols;
 
@@ -116,34 +233,31 @@ function drawBoard() {
             let rectX = col * rectW;
             let rectY = row * rectH;
 
+            /* Checker Pattern */
             if((row + col) % 2 == 0) {
                 ctx.fillStyle = "#FFFFFF";
             } else {
                 ctx.fillStyle = "#FFE194";
             }
             ctx.fillRect(rectX, rectY, rectW, rectH);
+
+            /* Algebraic Notation */
+            if(row == board.rows - 1) {
+                ctx.fillStyle = "#000000";
+                ctx.fillText(col + 1, rectX, rectY + rectH);
+            }
+            if(col == board.cols - 1) {
+                ctx.fillStyle = "#000000";
+                ctx.fillText(board.rows - row + 1, rectX, rectY);
+            }    
         }
     }
-
-    /* TODO: Algebraic Notation */
-
 }
 
 async function clearBoard() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-function drawImage(path, x, y) {
-    if(path) {
-        var img = new Image();
-        img.onload = function() {
-            ctx.drawImage(img, x, y, img.width*1.5, img.height*1.5);
-        }
-        img.src = path; 
-    }
-}
-
-/* TODO: remove fixed values for adjusting image position and size to the board */
 function drawPieces() {
     let rectW = canvas.width / board.rows;
     let rectH = canvas.height / board.cols;
@@ -154,8 +268,16 @@ function drawPieces() {
             let rectY = row * rectH;
             let PieceToDraw = PieceImages[board.square[row][col]];
 
-            if (PieceToDraw) {
-                drawImage(PieceToDraw, rectX + rectW/16, rectY + rectH/16);
+            if(PieceToDraw) {
+                let img = new Image();
+                img.onload = function() {
+                    let w = rectW / img.naturalWidth;
+                    let h = rectH / img.naturalHeight;
+                    let x = rectX + w;
+                    let y = rectY + h;
+                    ctx.drawImage(img, x, y, img.width * w, img.height * h);
+                }
+                img.src = PieceToDraw; 
             }
         }
     }
@@ -169,19 +291,10 @@ async function draw() {
     drawPieces();
 }
 
-function Init(){
-    board.Init();
-    loadFEN();
-}
-
+/* Main Loop */
 Init();
-// pieces[1].Move(new Position(2, 2));
-// draw();
-// pieces[1].Move(new Position(3, 3));
-// draw();
-// pieces[1].Move(new Position(2, 4));
-// draw();
-// pieces[3].Move(new Position(2, 6));
+board.Move("c7c5");
+board.Move("c1c5");
+board.Display();
 draw();
-genFEN();
-console.log(pieces);
+let newFEN = genFEN();
